22ea45d455f72c98822b7d4c2d0cf46cffeca929
german <german.kisin.box@gmail.com>
2024-10-09 19:45:49 +0200

    02.10.2024 Start Git!

---
+++ lesson_20/code/classwork_20/application/ArgAppl.java
@@ -0,0 +1,21 @@
+package classwork_20.application;
+
+public class ArgAppl {
+    public static void main(String[] args) {
+
+        //метод main получает аргументы
+        //Snring[] args - это массив данных типа стринг, args - это ИМЯ массива
+
+        String str1 = args[0];
+        //int n1 = Integer.parseInt(str1);
+
+        String str2 = args[1];
+        //int n2 = Integer.parseInt(str2);
+
+        //System.out.println(str1+" * " + str2 + " = " + n1 * n2);
+        System.out.println(str1 + " and " + str2);
+
+
+
+    }
+}
